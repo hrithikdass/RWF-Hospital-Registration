9f4e03717a98e1d1590923b7fb005c3372d8cb57
hrithikdass <hrithikdas@gmail.com>
2021-08-17 17:10:05 +0530

    registrationd layout completed

--- lib/registerpage.dart
+++ lib/registerpage.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:rwf_hospital_registration/patientloginpage.dart';
 import 'constants.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -11,6 +12,8 @@ class RegisterPage extends StatefulWidget {
   _RegisterPageState createState() => _RegisterPageState();
 }
 
+bool securetext = true;
+
 class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
@@ -18,61 +21,141 @@ class _RegisterPageState extends State<RegisterPage> {
       appBar: AppBar(
         title: Text('Hospital Management'),
       ),
-      body: Column(
-        children: [
-          Container(
-            child: Column(
-              children: [
-                Text(
-                  'OPD ID',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    color: kTextColor,
-                    fontSize: 18.0,
-                  ),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Enter the OPD ID',
-                    border: UnderlineInputBorder(),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            child: Column(
-              children: [
-                Text(
-                  'DOB',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    color: kTextColor,
-                    fontSize: 18.0,
-                  ),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'DD/MM/YYYY',
-                    border: UnderlineInputBorder(),
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        Icons.calendar_today_outlined,
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(7.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'OPD ID',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: kTextColor,
+                        fontSize: 18.0,
                       ),
-                      onPressed: () {
-                        showDatePicker(
-                            context: context,
-                            initialDate: DateTime.now(),
-                            firstDate: DateTime(1900),
-                            lastDate: DateTime.now());
-                      },
                     ),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Enter the OPD ID',
+                        border: UnderlineInputBorder(),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(7.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'DOB',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: kTextColor,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: 'DD/MM/YYYY',
+                        border: UnderlineInputBorder(),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            Icons.calendar_today_outlined,
+                          ),
+                          onPressed: () {
+                            showDatePicker(
+                                context: context,
+                                initialDate: DateTime.now(),
+                                firstDate: DateTime(1900),
+                                lastDate: DateTime.now());
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(7.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'Phone Number',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: kTextColor,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(prefixText: '+91'),
+                      keyboardType: TextInputType.number,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(7.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'Password',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: kTextColor,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        suffixIcon: IconButton(
+                          icon: Icon(securetext
+                              ? Icons.visibility_off
+                              : Icons.visibility),
+                          onPressed: () {
+                            setState(() {
+                              securetext = !securetext;
+                            });
+                          },
+                        ),
+                      ),
+                      obscureText: securetext,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(25.0),
+                child: SizedBox(
+                  height: 50.0,
+                  width: 150.0,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: kButtonColor, // background
+                      onPrimary: Colors.black, // foreground
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return PatientLoginPage();
+                          },
+                        ),
+                      );
+                    },
+                    child: Text('SIGN UP'),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
